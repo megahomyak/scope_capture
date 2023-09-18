522c6d41621072f666d83d0a00d96a41c6235a69
megahomyak <g.megahomyak@gmail.com>
2023-09-18 23:27:37 +0500

    No breakthrough, it seems like...

--- scope_capture/__init__.py
+++ scope_capture/__init__.py
@@ -7,15 +7,13 @@ def capture(f):
         fn_globals = {}
         fn_globals.update(f.__globals__)
         fn_globals.update(frame.f_back.f_locals)
-        call_fn = FunctionType(getattr(f, "__code__"), fn_globals)
+        call_fn = FunctionType(
+            code=f.__code__,
+            globals=fn_globals,
+            name=f.__name__,
+            argdefs=f.__defaults__,
+            closure=f.__closure__,
+        )
         return call_fn
     finally:
         del frame # See note at https://docs.python.org/3/library/inspect.html#the-interpreter-stack
-
-if __name__ == "__main__":
-    a = []
-    SOMETHING_GLOBAL = 123
-    for i in [1, 2]:
-        a.append(capture(lambda: (SOMETHING_GLOBAL, i)))
-
-    print(a[0](), a[1]())
---
+++ test_scope_capture.py
@@ -0,0 +1,25 @@
+from scope_capture import capture
+
+
+def test_capturing_in_a_lambda():
+    a = []
+    SOMETHING_GLOBAL = 123
+    for i in [1, 2]:
+        a.append(capture(lambda: (SOMETHING_GLOBAL, i)))
+    assert [f() for f in a] == [(123, 1), (123, 2)]
+
+
+def test_accepting_arguments():
+    assert capture(lambda a: a + 2)(2) == 4
+    assert capture(lambda a=3: a + 2)() == 5
+    assert capture(lambda a: a + 2)(a=4) == 6
+
+
+def test_capturing_in_a_function():
+    a = []
+    SOMETHING_GLOBAL = 256
+    for i in [3, 4]:
+        def f():
+            return (SOMETHING_GLOBAL, i)
+        a.append(capture(f))
+    assert [f() for f in a] == [(256, 3), (256, 4)]
