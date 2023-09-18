e0259202df0cc62bee7d48394b51c4c9b277bc73
megahomyak <g.megahomyak@gmail.com>
2023-09-18 23:05:01 +0500

    It works!!!

---
+++ scope_capture/__init__.py
@@ -0,0 +1,20 @@
+import inspect
+from types import FunctionType
+
+def capture(f):
+    try:
+        frame = inspect.currentframe()
+        fn_globals = {}
+        fn_globals.update(f.__globals__)
+        fn_globals.update(frame.f_back.f_locals)
+        call_fn = FunctionType(getattr(f, "__code__"), fn_globals)
+        return call_fn
+    finally:
+        del frame # See note at https://docs.python.org/3/library/inspect.html#the-interpreter-stack
+
+a = []
+SOMETHING_GLOBAL = 123
+for i in [1, 2]:
+    a.append(capture(lambda: (SOMETHING_GLOBAL, i)))
+
+print(a[0](), a[1]())
